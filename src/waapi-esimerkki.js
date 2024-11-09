be04cbd9d1825f21f8b315a23e444e4222431c6e
Juho Nuottimäki <67386247+jauhoja@users.noreply.github.com>
2024-11-09 17:24:53 +0200

    make button that plays sound

--- src/waapi-esimerkki.js
+++ src/waapi-esimerkki.js
@@ -1,20 +1,32 @@
 // 1. Luodaan uusi audiokonteksti-objekti
 const audioCtx = new AudioContext();
+document.getElementById('button').addEventListener('click', () => {
 
-// 2. Audiolahteen luominen
-const audioElement = document.querySelector('audio');
-const sourceNode = audioCtx.createMediaElementSource(audioElement);
+  // 2. Audiolahteen luominen
+  const sourceNode = audioCtx.createOscillator();
+  sourceNode.type = 'square';
+  sourceNode.frequency.value = 440;
 
-// 3. Audiosignaalin voimakkuuden lasku
-const gainNode = audioCtx.createGain();
-gainNode.gain.value = 0.5;
+  // 3. Audiosignaalin voimakkuuden lasku
+  const gainNode = audioCtx.createGain();
+  gainNode.gain.value = 0.5;
 
-// 4. Audion suodatus
-const filterNode = audioCtx.createBiquadFilter();
-filterNode.type = 'lowpass';
-filterNode.frequency.value = 1000;
+  // 4. Audion suodatus
+  const filterNode = audioCtx.createBiquadFilter();
+  filterNode.type = 'lowpass';
+  filterNode.Q.value = 10;
+  filterNode.frequency.setValueAtTime(1000, audioCtx.currentTime);
+  filterNode.frequency.exponentialRampToValueAtTime(10, audioCtx.currentTime + 1);
 
-// 5. Solmujen kytkeminen
-sourceNode.connect(gainNode);
-gainNode.connect(filterNode);
-filterNode.connect(audioCtx.destination);
+
+  // 5. Solmujen kytkeminen
+  sourceNode.connect(gainNode);
+  gainNode.connect(filterNode);
+  filterNode.connect(audioCtx.destination);
+
+// 6. Signaalin toisto
+  sourceNode.start();
+  setTimeout(() => {
+    sourceNode.stop();
+  }, 1000);
+});
